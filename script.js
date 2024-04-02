00e661ea1242a1c528a1b0a6ffdd815ad1e20a49
Malcom X <Taajlogan@gmail.com>
2024-04-02 16:02:47 -0400

    modified_HTML_andCSS

---
+++ script.js
@@ -0,0 +1,8 @@
+//Variables
+const timer = document.querySelector("p.clock");     
+const seconds = 90  
+const points = document.querySelector("#points");      
+const intro = document.querySelector("#intro");  
+const Q&A = document.querySelector("Q&A");  
+const timer = document.querySelector("questions");  
+let questionCount = 0;
